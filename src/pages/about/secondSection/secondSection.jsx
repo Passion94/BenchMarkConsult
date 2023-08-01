db6ea1876079c7f54c4d169f80efca97d80eb600
Ogodo Onyebuchi <whobuchim@gmail.com>
2023-08-01 21:54:34 +0100

    Made changes to about us background

--- src/pages/about/secondSection/secondSection.jsx
+++ src/pages/about/secondSection/secondSection.jsx
@@ -4,7 +4,7 @@ import { Box } from '@mui/material'
 
 export const SecondSection = () => {
   return (
-    <Box sx={{background:"rgba(51, 153, 255, 0.2)"}}>
+    <Box >
         <AreaOfFocus/>
     </Box>
   )
